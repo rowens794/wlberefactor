d69058aec87649c15b55f48ea5cfef4daee22707
Ryan Owens <rowens794@gmail.com>
2018-12-20 11:21:55 -0500

    fixed a broken link in registerfrominvite component

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -4,31 +4,11 @@ exports.sendWelcomeEmail = function (email, userID, name, verificationString, co
     // using SendGrid's v3 Node.js Library
     // https://github.com/sendgrid/sendgrid-nodejs
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    // console.log('----------mailController.sendWelcomeEmail-----------')
+    console.log('----------mailController.sendWelcomeEmail-----------')
 
-    // if (competitionID != undefined){
-    //     var htmlString = `<strong>Welcome to Big Loosers!</strong><br /><p>Starting and joining a Weightloss competition with your friends and family is easy. All that you need to do to get start is click the following registration link to confirm your email address.</p><a href=${process.env.SERVER_URL + '/userVerification/' + userID + '/' + verificationString + '?comp=' + competitionID}>verify email`
-    // }
-    // else{
-    //     var htmlString = `<strong>Welcome to Big Loosers!</strong><br /><p>Starting and joining a Weightloss competition with your friends and family is easy. All that you need to do to get start is click the following registration link to confirm your email address.</p><a href=${process.env.SERVER_URL + '/userVerification/' + userID + '/' + verificationString}>verify email`
-    // }
-
-    // const msg = {
-    //     to: email,
-    //     from: 'Ryan@bigloosers.com',
-    //     subject: 'Welcome to Big Loosers - Verification',
-    //     text: `Welcome to Big Loosers! Starting and joining a Weightloss competition with your friends and family is easy. All that you need to do to get start is click the following registration link to confirm your email address. ${process.env.SERVER_URL + 'userVerification' + userID + '/' + verificationString}`,
-    //     html: htmlString    
-    // };
-
-    // sgMail.send(msg, (error, msg) => {
-    //     if(error){
-    //         console.log('0')
-    //     }else{
-    //         console.log('confirmation message sent successfully to ')
-    //         console.log(email)
-    //     }
-    // });
+    if (competitionID != undefined){
+        verificationString = verificationString + '?comp=' + competitionID
+    }
 
     const msg = {
         to: email,
